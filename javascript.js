9b5e3c87827f52e7b0565184fa1ce52a6a67bce2
Dhanyashree <75880251+dhanyashree-karkera@users.noreply.github.com>
2024-09-04 15:56:09 +0530

    javascript.js

---
+++ javascript.js
@@ -0,0 +1,11 @@
+let name='Dhanya';
+let name1=' Karkera';
+console.log(name.length);                //6
+console.log(name.toUpperCase());        //DHANYA
+console.log(name.toLowerCase());        //dhanya
+console.log(name.slice(1))              //hanya
+console.log(name.replace('a','n'));     //Dhnnya
+console.log(name.concat(name1));        //Dhanya Karkera
+console.log(name1.trim());              //Karkera
+console.log(name[0]);                   //D
+console.log("har\"".length)             //4
